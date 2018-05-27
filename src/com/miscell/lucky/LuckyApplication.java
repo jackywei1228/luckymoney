a43dae3220ae4f92e7393650a7be35b76fcad0be
jackywei <jackywei1228@gmail.com>
2018-05-27 16:59:13 +0800

    init

--- src/com/miscell/lucky/LuckyApplication.java
+++ src/com/miscell/lucky/LuckyApplication.java
@@ -1,7 +1,7 @@
 package com.miscell.lucky;
 
 import android.app.Application;
-import com.flurry.android.FlurryAgent;
+//import com.flurry.android.FlurryAgent;
 
 /**
  * Created by chenjishi on 15/2/12.
@@ -11,7 +11,6 @@ public class LuckyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        FlurryAgent.init(this, "37T6NSWDQZ28FNJSWYBB");
+        //FlurryAgent.init(this, "37T6NSWDQZ28FNJSWYBB");
     }
 }
